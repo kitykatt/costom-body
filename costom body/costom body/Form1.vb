2b6282a9ef160b489c0186e5e23c7a4477b11b36
kitykatt <90800597+kitykatt@users.noreply.github.com>
2022-01-23 18:26:03 -0700

    Add files via upload

--- costom body/costom body/Form1.vb
+++ costom body/costom body/Form1.vb
@@ -9,99 +9,69 @@
     Dim a7 As Integer
     Dim a8 As Integer
     Dim a9 As Integer
-    Private Sub Buttonmash_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click
-        Label1.Text = money
-    End Sub
+    Dim b1 As Integer
+    Dim b2 As Integer
+    Dim b3 As Integer
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If a2 Or a3 = 1 Then
-            a1 = 1
-            a2 = 0
-            a3 = 0
-        End If
+        b1 = 30
         PictureBox1.Image = Button1.BackgroundImage
-        money = money + 30
+        money = b1 + b2 + b3
+        Label1.Text = money
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        If a2 = 0 Then
-            PictureBox1.Image = Button2.BackgroundImage
-            money = money + 100
-            a1 = 1
-        End If
-        If a1 Or a3 = 1 Then
-            a2 = 1
-            a2 = 0
-            a3 = 0
-        End If
+        b1 = 100
+        PictureBox1.Image = Button2.BackgroundImage
+        money = b1 + b2 + b3
+        Label1.Text = money
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        If a1 Or a2 = 1 Then
-            a1 = 0
-            a2 = 0
-            a3 = 1
-        End If
+        b1 = 15
         PictureBox1.Image = Button3.BackgroundImage
-        money = money + 50
+        money = b1 + b2 + b3
+        Label1.Text = money
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        If a2 Or a3 = 1 Then
-            a1 = 1
-            a2 = 0
-            a3 = 0
-            PictureBox2.Image = Button4.BackgroundImage
-            money = money + 30
-        End If
+        b2 = 14
+        PictureBox2.Image = Button4.BackgroundImage
+        money = b1 + b2 + b3
+        Label1.Text = money
     End Sub
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
-        If a2 Or a3 = 1 Then
-            a1 = 1
-            a2 = 0
-            a3 = 0
-            PictureBox2.Image = Button5.BackgroundImage
-            money = money + 30
-        End If
+        b2 = 25
+        PictureBox2.Image = Button5.BackgroundImage
+        money = b1 + b2 + b3
+        Label1.Text = money
     End Sub
 
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
-        If a2 Or a3 = 1 Then
-            a1 = 1
-            a2 = 0
-            a3 = 0
-            PictureBox2.Image = Button6.BackgroundImage
-            money = money + 30
-        End If
+        b2 = 7
+        PictureBox2.Image = Button6.BackgroundImage
+        money = b1 + b2 + b3
+        Label1.Text = money
     End Sub
 
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
-        If a2 Or a3 = 1 Then
-            a1 = 1
-            a2 = 0
-            a3 = 0
-            PictureBox3.Image = Button7.BackgroundImage
-            money = money + 70
-        End If
+        b3 = 70
+        PictureBox3.Image = Button7.BackgroundImage
+        money = b1 + b2 + b3
+        Label1.Text = money
     End Sub
 
     Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
-        If a2 Or a3 = 1 Then
-            a1 = 1
-            a2 = 0
-            a3 = 0
-            PictureBox3.Image = Button8.BackgroundImage
-            money = money + 60
-        End If
+        b3 = 60
+        PictureBox3.Image = Button8.BackgroundImage
+        money = b1 + b2 + b3
+        Label1.Text = money
     End Sub
 
     Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
-        If a7 Or a8 = 1 Then
-            a7 = 0
-            a8 = 0
-            a9 = 1
-            PictureBox3.Image = Button9.BackgroundImage
-            money = money + 80
-        End If
+        b3 = 90
+        PictureBox3.Image = Button9.BackgroundImage
+        money = b1 + b2 + b3
+        Label1.Text = money
     End Sub
 End Class
